4fafca63ee4fa6379459fdd65dd1978c539d0185
m-yoshiro <massugu.massugu@gmail.com>
2020-10-17 17:36:57 +0900

    implemented basic functions

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import selector2Regexp from "selector-2-regexp";
 
 document.addEventListener('DOMContentLoaded', () => {
-  const result = document.querySelector('.result');
+  const result = document.getElementById('result');
   const inputSelector = document.getElementById('selector');
+  const copyButton = document.querySelector('.copy');
 
   inputSelector.addEventListener('input', (evt) => {
     const { target } = evt;
@@ -20,7 +21,30 @@ document.addEventListener('DOMContentLoaded', () => {
         console.error(error);
       }
     }
-
     result.textContent = output;
+  });
+
+  copyButton.addEventListener('click', (evt) => {
+    const { target } = evt;
+
+    if (!(target instanceof HTMLButtonElement)) {
+      throw new Error("butttttttt");
+    }
+
+    if (!target.dataset.target) {
+      throw new Error("butttttttt");
+    }
+
+    const targetId = target.dataset.target;
+    const resource = document.getElementById(targetId);
+
+    if (!resource) {
+      throw new Error("butttttttt");
+    }
+    
+    navigator.clipboard.writeText(resource.textContent).then(
+      () => { console.log('copied!!')},
+      () => {}
+    );
   })
 })
